913a6e26da6915ae423b5a90754815422254f61b
matvik777 <2805matvik4@gmail.com>
2025-03-05 21:22:36 +0300

    add study

--- pr_1/machine_state.py
+++ pr_1/machine_state.py
@@ -32,7 +32,29 @@ class MachineState:
             "speed": self.speed,
             "laser_on": self.laser_on
         }
-        
+    def bresenham_line(self, x1, y1, x2, y2):
+        """Алгоритм Брезенхэма: возвращает список точек для движения"""
+        points = []
+        dx = abs(x2 - x1)
+        dy = abs(y2 - y1)
+        sx = 1 if x1 < x2 else -1
+        sy = 1 if y1 < y2 else -1
+        err = dx - dy
+
+        while True:
+            points.append((x1, y1))
+            if x1 == x2 and y1 == y2:
+                break
+            e2 = 2 * err
+            if e2 > -dy:
+                err -= dy
+                x1 += sx
+            if e2 < dx:
+                err += dx
+                y1 += sy
+
+        return points
+      
 if __name__ == "__main__":
     machine = MachineState()
     
